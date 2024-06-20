42e8298d1b0278cb4fb53cedb1da0b6d85fa0473
Farrel Kardia <fmkardia@gmail.com>
2024-06-20 10:01:15 +0700

    update

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,3 +1,14 @@
 class Controller{
-    
+    static landingpage(req,res){
+        try {
+            res.render("landingPage")
+            
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static 
 }
+
+module.exports = Controller
